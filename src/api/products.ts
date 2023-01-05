e3ea9bea692db74c1a129561772450d46c6559a6
Sedrak Adoyan <set.adoyan@gmail.com>
2023-01-05 17:54:17 +0400

    feat: add products api

---
+++ src/api/products.ts
@@ -0,0 +1,16 @@
+import IProducts from "../interfaces/IProducts";
+import { ProductType } from "../types/productType";
+
+class Products {
+  getAll = async (): Promise<IProducts> => {
+    const res: IProducts = await fetch("https://dummyjson.com/products?limit=100").then(res => res.json());
+    return res;
+  };
+
+  getOneById = async (id: number): Promise<ProductType> => {
+    const res = await fetch(`https://dummyjson.com/products/${id}`).then(res => res.json());
+    return res;
+  }
+}
+
+export const products = new Products();
