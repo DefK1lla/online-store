672f233646e6407500c87d9c5b45a07dfce12d50
Sedrak Adoyan <set.adoyan@gmail.com>
2023-01-09 19:50:51 +0400

    feat: add empty products page

--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Component } from "react";
 import { ProductItem } from "./ProductItem";
 import styles from "./style.module.scss";
@@ -9,22 +9,32 @@ export class ProductList extends Component<IListProps>{
   render() {
     return (
       <>
-        <Grid
-          className={styles.listContainer}
-          container
-          spacing={{ xs: 2, md: 3 }}
-        >
-          {this.props.products.map((product: ProductType) => (
-            <Grid item
-              xs={12} sm={6} md={4} lg={4}
-              key={product.id}
-              className={styles.listContainer_item}>
-              <ProductItem
-                product={product}
-              />
-            </Grid>
-          ))}
-        </Grid>
+        {this.props.products.length > 0 &&
+          <Grid
+            className={styles.listContainer}
+            container
+            spacing={{ xs: 2, md: 3 }}
+          >
+            {this.props.products.map((product: ProductType) => (
+              <Grid item
+                xs={12} sm={6} md={4} lg={4}
+                key={product.id}
+                className={styles.listContainer_item}>
+                <ProductItem
+                  product={product}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        }
+
+        {!this.props.products.length &&
+          <Typography
+            variant="h3"
+          >
+            No products found
+          </Typography>
+        }
       </>
     )
   }
